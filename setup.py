204a2aa2fe94913cd4451382c8c6d0ccce64184a
Maël BABERT <m.babert@workstation3.nsi.lan>
2022-10-18 15:14:08 +0200

    beginning of the kernel of the game, struggle with the move function

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hanoi_kernel',
+    version='0.1',
+    url='https://github.com/babert-Mael/hanoi_kernel',
+    author='Babert Maël',
+    author_email='babert-m@saint-louis29.net',
+    description='The core of the hanoi game',
+    packages=find_packages(),   
+    install_requires=['Stack'],
+)
